39cd3e1bafd5efd2d19195ce73e504426061c583
Max Beutel <me@maxbeutel.de>
2015-09-20 09:39:41 +0000

    prepare api change for dynamic array, keep track on length and elements added separately, removed some functions that only bloat the api for now

--- exercise-32/c-skeleton/src/liblcthw/dynamic_array.c
+++ exercise-32/c-skeleton/src/liblcthw/dynamic_array.c
@@ -6,20 +6,21 @@
 
 #include "liblcthw/dynamic_array.h"
 
+// debug, in order to avoid compiler warnings for not yet implemented functions
 #define UNUSED(x) (void)(x)
 
-DArray *DArray_create(size_t element_size, size_t initial_max)
+DArray *DArray_create(size_t element_size, size_t length)
 {
-    assert(initial_max > 0 && "You must set an initial_max > 0.");
+    assert(length > 0 && "You must set a length > 0.");
 
     DArray *array = malloc(sizeof(DArray));
     assert(array != NULL);
-    array->max = initial_max;
+    array->length = length;
+    array->elements_added = 0;
 
-    array->contents = calloc(initial_max, sizeof(void *));
+    array->contents = calloc(length, sizeof(void *));
     assert(array->contents != NULL); // @FIXME goto und free(array)
-    
-    array->end = 0;
+
     array->element_size = element_size;
     array->expand_rate = DEFAULT_EXPAND_RATE;
 
@@ -49,20 +50,23 @@ int DArray_contract(DArray *array)
     return -1;
 }
 
+// @FIXME why does this not expand?
+// @FIXME this must expand if elements_added == length
 int DArray_push(DArray *array, void *el)
 {
     assert(array != NULL);
 
-    array->contents[array->end] = el;
-    array->end++;
+    array->contents[array->length] = el;
 
-    return 0;
+    return 0; // does a return value make sense here?
 }
 
+// @FIXME this must then resize (contract) ?
 void *DArray_pop(DArray *array)
 {
-    void *value = array->contents[(array->end - 1)];
-    array->end--;
+    assert(array != NULL);
+
+    void *value = array->contents[(array->length - 1)];
     
     return value;
 }
--- exercise-32/c-skeleton/src/liblcthw/dynamic_array.h
+++ exercise-32/c-skeleton/src/liblcthw/dynamic_array.h
@@ -5,14 +5,16 @@
 #include <assert.h>
 
 typedef struct DArray {
-    int end;
-    int max;
+    // current length of the array
+    int length;
+    // number of elements that have been added so far (@FIXME possible buffer overflow)
+    int elements_added;
     size_t element_size;
     size_t expand_rate;
     void **contents;
 } DArray;
 
-DArray *DArray_create(size_t element_size, size_t initial_max);
+DArray *DArray_create(size_t element_size, size_t length);
 void DArray_destroy(DArray *array);
 
 int DArray_expand(DArray *array);
@@ -21,51 +23,32 @@ int DArray_contract(DArray *array);
 int DArray_push(DArray *array, void *el);
 void *DArray_pop(DArray *array);
 
-#define DArray_last(A) ((A)->contents[(A)->end - 1])
-#define DArray_first(A) ((A)->contents[0])
-#define DArray_end(A) ((A)->end)
-#define DArray_count(A) DArray_end(A)
-#define DArray_max(A) ((A)->max)
-
 #define DEFAULT_EXPAND_RATE 300
 
 // @FIXME why does this not expand?
-static inline void DArray_set(DArray *array, int i, void *el)
+static inline void DArray_set(DArray *array, int index, void *el)
 {
-    assert(i < array->max && "DArray attempt to get past max.");
+    assert(index < array->length && "DArray attempt to set undefined index.");
     
-    if (i > array->end) {
-        array->end = i;
-    }
-    
-    array->contents[i] = el;
+    array->contents[index] = el;
 }
 
-static inline void *DArray_get(DArray *array, int i)
+static inline void *DArray_get(DArray *array, int index)
 {
-    assert(i < array->max && "DArray attempt to get past max.");
-    
+    assert(index < array->max && "DArray attempt to get undefined index.");
+
     return array->contents[i];
 }
 
-static inline void *DArray_remove(DArray *array, int i)
+static inline void *DArray_remove(DArray *array, int index)
 {
-    assert(i < array->max && "DArray attempt to get past max.");
+    assert(index < array->max && "DArray attempt to remove undefined index.");
     
-    void *el = array->contents[i];
+    void *el = array->contents[index];
     
-    array->contents[i] = NULL;
+    array->contents[index] = NULL;
     
     return el;
 }
-
-// why do we actually need this?
-// #define DArray_free(E) free((E))
-/*static inline void *DArray_new(DArray *array)
-{
-    assert(array->element_size > 0 && "Can't use DArray_new on 0 size darrays.");
-    
-    return calloc(1, array->element_size);
-}*/
 
 #endif
